4ac45c3995547a180f652a8fdf70626ce79bddc8
ValJeansEvilBread <64847510+ValJeansEvilBread@users.noreply.github.com>
2020-07-14 15:34:00 -0400

    initial commit

---
+++ src/main/java/com/tts/TTSTwitter/JavascriptBobApplication.java
@@ -0,0 +1,13 @@
+package com.tts.TTSTwitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavascriptBobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavascriptBobApplication.class, args);
+	}
+
+}
